f5d24e350eb6ca00cb732d436d110e625ef632da
Bret <73080023+bretThinkful@users.noreply.github.com>
2021-01-14 10:29:20 -0700

    Update App.js

--- starwars-api/src/App.js
+++ starwars-api/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Seach from './Search';
 import './App.css';
 
 class App extends React.Component {
@@ -7,14 +8,43 @@ class App extends React.Component {
     API_URL : "https://swapi-thinkful.herokuapp.com/api/people/?search=",
     characters : []
   }
+  // setup handleSubmit function to fetch API
+  handleSubmit = (event) => {
+    // setup preventDefault
+    event.preventDefault();
+    // create variable for characterName value
+    const character = event.target.characterName.value;
+    // setup fetch to grab the starwars API
+    fetch(this.state.API_URL + character)
+      .then(response => {
+        if(response.ok) {
+          return response.json();
+        }
+      })
+      .then(responseJson => {
+        this.setState({
+          characters : responseJson.results
+        });
+      });
+
+  }
 
   render() {
     return (
       <div className="App">
-        <h2>Name</h2>
+        {/*Attach handleSubmit to search component*/}
+        <Search handleSubmit={this.handleSubmit}/>
+
+        {this.state.characters.map((character, index) => {
+          return (
+            <div>
+              <h2>{character.name}</h2>
+            </div>
+          )
+        })}
       </div>
-    )
+    );
   }
-}
+}  
 
 export default App;
